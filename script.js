5982baddf0a193b529991974d8c0eaec3d394825
jmjcodez <johnmjones94@gmail.com>
2025-01-03 21:27:49 -0600

    Add function for grid resizing and extra credit Implements hover effect for random colors, and increased opacity,as well as a re-sizible grid based on user input

--- script.js
+++ script.js
@@ -1,19 +1,60 @@
 const gridBox = document.querySelector("#grid");
-const squareSize = 25;
-
-
-makeSquare(squareSize);
-const gridSquare = document.querySelectorAll(".square").forEach(gridSquare => gridSquare.addEventListener("mouseenter",()=>{gridSquare.classList.add("hover")}));
-
-
-function makeSquare(squareSize){
-	for (i = 0; i < squareSize; i++){
-		const column = document.createElement("div");
-		for (j = 0; j < squareSize; j++){
-			const row = document.createElement("div");
-			row.classList.add("square");
-			column.appendChild(row);
-		}
-		gridBox.appendChild(column);
+let squareQty = 16;
+const containerSize = 700;
+let squareSize = containerSize / squareQty;
+makeSquare(squareQty);
+const gridSquare = document.querySelectorAll(".square").forEach(gridSquare => gridSquare.addEventListener("mouseenter",()=>{gridSquare.style.backgroundColor= random_bg_color();
+	let currentOpacity = parseFloat(gridSquare.style.opacity) || 0;
+	if (currentOpacity < 1) {
+			gridSquare.style.opacity = (currentOpacity + 0.1).toString();
 	}
-};
+}));
+const sizeBox = document.querySelector("#gridSize");
+
+sizeBox.addEventListener("click", ()=> {
+		let keepGoing = true;
+		while (keepGoing){
+			let gridInput = prompt("Select Grid Size 1-100",);
+			console.log(typeof gridInput);
+			console.log(parseInt(gridInput));
+			if (parseInt(gridInput) < 0 || parseInt(gridInput) > 100){
+				alert("ERROR: Grid size must be a number between 0-100");
+			}
+			else {
+				newGrid = parseInt(gridInput);
+				keepGoing = false;}
+		}
+		console.log (typeof newGrid);
+	 	while (gridBox.firstChild) {
+			gridBox.removeChild(gridBox.firstChild);}
+			squareSize = containerSize / newGrid;
+			makeSquare(newGrid);
+			const gridSquare = document.querySelectorAll(".square").forEach(gridSquare => gridSquare.addEventListener("mouseenter",()=>{gridSquare.style.backgroundColor= random_bg_color();}));
+		
+});
+
+
+
+function makeSquare(squareQty){
+	for (i = 0; i < squareQty; i++){
+		const row = document.createElement("div");
+		for (j = 0; j < squareQty; j++){
+		const square = document.createElement("div");
+		square.classList.add("square");
+		square.style.width = `${squareSize}px`;
+		square.style.height = `${squareSize}px`;
+		row.appendChild(square);
+		}
+		gridBox.appendChild(row);
+		}
+	};
+
+	function random_bg_color() {
+    var x = Math.floor(Math.random() * 256);
+    var y = Math.floor(Math.random() * 256);
+    var z = Math.floor(Math.random() * 256);
+    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+ console.log(bgColor);
+  
+    return bgColor;
+    }
