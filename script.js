94a08a4400464767c3bdc743649966902c2b0f6f
jmjcodez <johnmjones94@gmail.com>
2025-01-03 21:31:31 -0600

    Update grid resize function to include opacity function

--- script.js
+++ script.js
@@ -15,8 +15,7 @@ sizeBox.addEventListener("click", ()=> {
 		let keepGoing = true;
 		while (keepGoing){
 			let gridInput = prompt("Select Grid Size 1-100",);
-			console.log(typeof gridInput);
-			console.log(parseInt(gridInput));
+			
 			if (parseInt(gridInput) < 0 || parseInt(gridInput) > 100){
 				alert("ERROR: Grid size must be a number between 0-100");
 			}
@@ -24,12 +23,17 @@ sizeBox.addEventListener("click", ()=> {
 				newGrid = parseInt(gridInput);
 				keepGoing = false;}
 		}
-		console.log (typeof newGrid);
+
 	 	while (gridBox.firstChild) {
 			gridBox.removeChild(gridBox.firstChild);}
 			squareSize = containerSize / newGrid;
 			makeSquare(newGrid);
-			const gridSquare = document.querySelectorAll(".square").forEach(gridSquare => gridSquare.addEventListener("mouseenter",()=>{gridSquare.style.backgroundColor= random_bg_color();}));
+			const gridSquare = document.querySelectorAll(".square").forEach(gridSquare => gridSquare.addEventListener("mouseenter",()=>{gridSquare.style.backgroundColor= random_bg_color();
+				let currentOpacity = parseFloat(gridSquare.style.opacity) || 0;
+				if (currentOpacity < 1) {
+						gridSquare.style.opacity = (currentOpacity + 0.1).toString();
+				}
+			}));
 		
 });
 
